aca0a37c7d9f71e041e62df43731d603aaa04269
Nathan McDonald <nmcdonald00@gmail.com>
2013-02-18 20:21:29 -0400

    added new function to change query without having to reconstruct the object

--- XPathKit.js
+++ XPathKit.js
@@ -3,52 +3,68 @@ function XPathKit(myXmlObject,myPath){
 	this.path = myPath;
 	this.xmlObject = myXmlObject;
 	this.count = 0;
+	this.nodes;
 
 	var i = -1;
 	if(window.ActiveXObject){//activexobject doesnt exist unless it;s ie
 		this.xmlObject.setProperty("SelectionLanguage","XPath"); // xpath sorts through arrays starting at one this sets it so ie works that way too
-		var nodes = this.xmlObject.selectNodes(path);
+		this.nodes = this.xmlObject.selectNodes(path);
 		this.count = nodes.length;
 	}else{
 		//evaluate the xpath xpression
 		this.count = this.xmlObject.evaluate("count("+path+")",this.xmlObject,null,XPathResult.ANY_TYPE,null).numberValue;
 		//returns an xpath result object
-		var nodes = this.xmlObject.evaluate(path,this.xmlObject,null,XPathResult.ANY_TYPE,null);
+		this.nodes = this.xmlObject.evaluate(path,this.xmlObject,null,XPathResult.ANY_TYPE,null);
 	}
 
 	//---------------------------------------------------------METHODS
-	// this.newPath = function(myPath){
-	// 	this.path = myPath;
-	// 	if(window.ActiveXObject){//activexobject doesnt exist unless it;s ie
-	// 		this.xmlObject.setProperty("SelectionLanguage","XPath"); // xpath sorts through arrays starting at one this sets it so ie works that way too
-	// 		var nodes = this.xmlObject.selectNodes(path);
-	// 		this.count = nodes.length;
-	// 	}else{
-	// 		//evaluate the xpath xpression
-	// 		this.count = this.xmlObject.evaluate("count("+path+")",this.xmlObject,null,XPathResult.ANY_TYPE,null).numberValue;
-	// 		console.log
-	// 		//returns an xpath result object
-	// 		var nodes = this.xmlObject.evaluate(path,this.xmlObject,null,XPathResult.ANY_TYPE,null);
-	// 	}
-	// }
 
 	this.read = function(){
 
 		if(window.ActiveXObject){//activexobject doesnt exist unless it;s ie
 			i+=1;
 			if(!(i >= this.count)){
-				return nodes[i].childNodes[0].nodeValue;
+				return this.nodes[i].childNodes[0].nodeValue;
 			}else{
 				i = 0;
 			}
 		}else{
-			var result = nodes.iterateNext();
+			var result = this.nodes.iterateNext();
 			//interateNext() will return a false when there are no more nodes left 
 			return result.childNodes[0].nodeValue;
-			result = nodes.iterateNext();
+			result = this.nodes.iterateNext();
 		}
 		return false;
 	}
 
-	
+	this.newQuery = function(myPath){
+		this.path = myPath;
+		var i = -1;
+		if(window.ActiveXObject){//activexobject doesnt exist unless it;s ie
+			this.xmlObject.setProperty("SelectionLanguage","XPath"); // xpath sorts through arrays starting at one this sets it so ie works that way too
+			this.nodes = this.xmlObject.selectNodes(myPath);
+			this.count = this.nodes.length;
+		}else{
+			//evaluate the xpath xpression
+			this.count = this.xmlObject.evaluate("count("+myPath+")",this.xmlObject,null,XPathResult.ANY_TYPE,null).numberValue;
+			//returns an xpath result object
+			this.nodes = this.xmlObject.evaluate(myPath,this.xmlObject,null,XPathResult.ANY_TYPE,null);
+		}
+	}
+
 }
+
+function query(myPath){
+		this.path = myPath;
+		var i = -1;
+		if(window.ActiveXObject){//activexobject doesnt exist unless it;s ie
+			this.xmlObject.setProperty("SelectionLanguage","XPath"); // xpath sorts through arrays starting at one this sets it so ie works that way too
+			this.nodes = this.xmlObject.selectNodes(myPath);
+			this.count = this.nodes.length;
+		}else{
+			//evaluate the xpath xpression
+			this.count = this.xmlObject.evaluate("count("+myPath+")",this.xmlObject,null,XPathResult.ANY_TYPE,null).numberValue;
+			//returns an xpath result object
+			return this.nodes = this.xmlObject.evaluate(myPath,this.xmlObject,null,XPathResult.ANY_TYPE,null);
+		}
+	}
